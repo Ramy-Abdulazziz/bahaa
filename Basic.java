a9f1c24aa35821ebaec518654d1cb4332b84fe13
Ramy <ramy@Kurama>
2023-10-03 20:24:00 -0400

    feat: added .java file to practice git commands

---
+++ Basic.java
@@ -0,0 +1,23 @@
+public class Basic {
+
+    public static void main(String... args) {
+
+        String name = "bahaa"; 
+
+        /**
+         * 
+         * Write the code to print your name one character at a time, each character starting on a new line, like
+         * 
+         * b
+         * a
+         * h
+         * a
+         * a
+         * 
+         * and push it to github
+         */
+        
+        
+
+    }
+}
